fd60490a66ebd0531692e6080708b2a14aadc866
Rfallon89 <rory_fallon@hotmail.co.uk>
2023-01-17 11:01:01 +0000

    articles by id replaced

--- app/router/articles-router.js
+++ app/router/articles-router.js
@@ -1,10 +1,12 @@
 const articlesRouter = require("express").Router();
 const {
   getArticles,
+  getArticle,
   getArticleComments,
 } = require("../controllers/app.articles.controller");
 
 articlesRouter.get("/", getArticles);
+articlesRouter.get("/:article_id", getArticle);
 articlesRouter.get("/:article_id/comments", getArticleComments);
 
 module.exports = articlesRouter;
