f38aee360affe8d376b628b8696bd576f99056d7
Rfallon89 <rory_fallon@hotmail.co.uk>
2023-01-17 12:03:21 +0000

    added catch block

--- app/controllers/app.users.controllers.js
+++ app/controllers/app.users.controllers.js
@@ -1,7 +1,9 @@
 const { fetchUsers } = require("../models/app.users.model");
 
 exports.getUsers = (req, res, next) => {
-  fetchUsers().then((users) => {
-    res.status(200).send({ users });
-  });
+  fetchUsers()
+    .then((users) => {
+      res.status(200).send({ users });
+    })
+    .catch(next);
 };
