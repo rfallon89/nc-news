3b3a48cedc0f96da7671d6e1c7d5e87fbac62803
Rfallon89 <rory_fallon@hotmail.co.uk>
2023-01-18 09:28:21 +0000

    added body to articles

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -41,6 +41,7 @@ describe("App API, /api", () => {
             articles.forEach((article) => {
               expect(typeof article.author).toBe("string");
               expect(typeof article.title).toBe("string");
+              expect(typeof article.body).toBe("string");
               expect(typeof article.article_id).toBe("number");
               expect(typeof article.topic).toBe("string");
               expect(typeof article.created_at).toBe("string");
@@ -69,6 +70,7 @@ describe("App API, /api", () => {
             articles.forEach((article) => {
               expect(typeof article.author).toBe("string");
               expect(typeof article.title).toBe("string");
+              expect(typeof article.body).toBe("string");
               expect(typeof article.article_id).toBe("number");
               expect(typeof article.topic).toBe("string");
               expect(typeof article.created_at).toBe("string");
--- app/models/app.articles.model.js
+++ app/models/app.articles.model.js
@@ -7,7 +7,7 @@ exports.fetchArticles = (
   next
 ) => {
   const queryValues = [];
-  let sql = `SELECT articles.article_id, articles.author, articles.title,articles.topic, articles.created_at,articles.votes,articles.article_img_url, COUNT(comments.comment_id)::INT AS comment_count
+  let sql = `SELECT articles.article_id, articles.author,articles.body, articles.title,articles.topic, articles.created_at,articles.votes,articles.article_img_url, COUNT(comments.comment_id)::INT AS comment_count
   FROM articles
   LEFT OUTER JOIN comments
   ON articles.article_id = comments.article_id`;
