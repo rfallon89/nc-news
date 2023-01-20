4596da94ad2014ae93dbf29820e63aefaf932cfc
Rfallon89 <rory_fallon@hotmail.co.uk>
2023-01-20 15:23:30 +0000

    pipeline

--- db/connection.js
+++ db/connection.js
@@ -5,6 +5,9 @@ require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
+}
 const config =
   ENV === "production"
     ? {
@@ -12,9 +15,5 @@ const config =
         max: 2,
       }
     : {};
-
-if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error("PGDATABASE or DATABASE_URL not set");
-}
 
 module.exports = new Pool(config);
