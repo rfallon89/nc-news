4c633f75caea68588c95258b1a20b9eb0496e43d
Rfallon89 <rory_fallon@hotmail.co.uk>
2023-02-06 10:25:52 +0000

    allow cross origin resource sharing

--- app/app.js
+++ app/app.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const app = express();
 const apiRouter = require("./router/api-router");
+const cors = require("cors");
 const {
   customError,
   sqlError,
   serverError,
   pathError,
 } = require("./error-handling");
-
+app.use(cors());
 app.use(express.json());
 app.use("/api", apiRouter);
 
